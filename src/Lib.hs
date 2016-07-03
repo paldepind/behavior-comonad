f1751a6f371476a37d6aae6707fc709d7be59f89
paldepind <simonfv@gmail.com>
2016-07-03 15:58:19 +0200

    Add tests

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,29 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Fix
+
+type Time = Integer
+
+type Stream a = Time -> a
+
+type StreamGen a = Time -> a
+
+delay :: a -> Stream a -> StreamGen (Stream a)
+delay x s t_start t_sample
+  | t_start == t_sample = x
+  | t_start < t_sample = s (t_sample - 1)
+  | otherwise = error "Premature sample!"
+
+start :: StreamGen (Stream a) -> Stream a
+start g = g 0
+
+generator :: Stream (StreamGen a) -> Stream a
+generator g t_sample = g t_sample t_sample
+
+stateful :: a -> (a -> a) -> StreamGen (Stream a)
+stateful x_0 f = mfix $ \str -> delay x_0 (f <$> str)
+
+streamTest :: Integer -> StreamGen (Stream a) -> [a]
+streamTest dur g = map (start g) [0..dur]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.Hspec
+
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "stateful" $ do
+    it "carries state" $ do
+      (streamTest 6 $ stateful 2 (+3)) `shouldBe` [2,5,8,11,14,17,20]
