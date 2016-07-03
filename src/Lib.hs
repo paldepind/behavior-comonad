5e41335c5b10cc5366d5773e32a6eda1eb149119
paldepind <simonfv@gmail.com>
2016-07-03 16:26:44 +0200

    Add timers

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE RecursiveDo #-}
+
 module Lib where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Fix
+import Data.Maybe
 
 type Time = Integer
 
@@ -27,3 +30,30 @@ stateful x_0 f = mfix $ \str -> delay x_0 (f <$> str)
 
 streamTest :: Integer -> StreamGen (Stream a) -> [a]
 streamTest dur g = map (start g) [0..dur]
+
+countdown :: String -> Int -> StreamGen (Stream (String, Maybe Int))
+countdown name t = do
+  let tick prev = do {t <- prev; guard (t > 0); return (t - 1)}
+  timer <- stateful (Just t) tick
+  return ((,) name <$> timer)
+
+timerSource :: [(String, Int, Int)] -> StreamGen (Stream [Stream (String, Maybe Int)])
+timerSource ts = do
+  let gen t = mapM (uncurry countdown) newTimers
+        where newTimers = [(n, v) | (n, v, st) <- ts, st == t]
+  cnt <- stateful 0 (+1)
+  return $ generator (gen <$> cnt)
+
+collection :: Stream [Stream a] -> (a -> Bool) -> StreamGen (Stream [a])
+collection source isAlive = mdo
+  coll <- liftA2 (++) source <$> delay [] coll'
+  let collWithVals = zip <$> (sequence =<< coll) <*> coll
+      collWithVals' = filter (isAlive . fst) <$> collWithVals
+      coll' = map snd <$> collWithVals'
+  return $ map fst <$> collWithVals'
+
+timers :: [(String, Int, Int)] -> StreamGen (Stream [(String, Int)])
+timers timerData = do
+  src <- timerSource timerData
+  getOutput <$> collection src (isJust . snd)
+    where getOutput = fmap (map (\(name, Just val) -> (name, val)))
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,3 +7,18 @@ main = hspec $ do
   describe "stateful" $ do
     it "carries state" $ do
       (streamTest 6 $ stateful 2 (+3)) `shouldBe` [2,5,8,11,14,17,20]
+  describe "counters" $ do
+    it "countdown" $ do
+      streamTest 5 $ countdown "foo" 4
+      `shouldBe` [("foo",Just 4),("foo",Just 3),("foo",Just 2),("foo",Just 1),("foo",Just 0),("foo",Nothing)]
+    it "timers" $ do
+      streamTest 8 $ timers [("a", 3, 0), ("b", 5, 1), ("c", 3, 1), ("d", 4, 3)]
+      `shouldBe`
+      [[("a",3)],[("b",5),("c",3),("a",2)],
+       [("b",4),("c",2),("a",1)],
+       [("d",4),("b",3),("c",1),
+        ("a",0)],[("d",3),("b",2),("c",0)],
+        [("d",2),("b",1)],
+        [("d",1),("b",0)],
+        [("d",0)],
+        []]
